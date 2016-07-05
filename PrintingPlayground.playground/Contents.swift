3d7fcd1a23a9dda1ded851ca6aa5b85775f750a8
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-05 12:35:10 -0400

    Having fun in the printing playground

---
+++ PrintingPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+var favoriteWord = "positivity"
+var favoriteColor = "Purple"
+var favoriteMusician = "Ghostface Killah"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+print("Hey Mom, here's some things you may or may not know about me. My favorite word is \(favoriteWord), my favorite color is \(favoriteColor) & my favorite musician is \(favoriteMusician)")
+
+let skyColor = "purple"
+let skySentence = "When I look up, I only see \(skyColor)"
+print(skySentence)
+
